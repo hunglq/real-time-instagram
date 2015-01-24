4e195ab5a134fe64fcc2139d70f7fa5c0a2f78a2
Hung <hunglq@hdwebsoft.com>
2015-01-24 09:51:15 +0700

    fix page hang

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
 	res.header("Access-Control-Allow-Credentials", "true")
-    
+    next();
 };
 
 
