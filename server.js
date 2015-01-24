469b5a165bb3ba8ef9c1fcb0d0fbd6d1759c0585
Hung <hunglq@hdwebsoft.com>
2015-01-24 10:30:10 +0700

    test

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ io.configure(function () {
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*:*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
 	res.header("Access-Control-Allow-Credentials", "true")
     return next();
