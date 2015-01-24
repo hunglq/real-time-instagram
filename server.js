f993bbb58f693f0c825e8a794438733311d19b0e
Hung <hunglq@hdwebsoft.com>
2015-01-24 10:06:24 +0700

    fix port

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ io.configure(function () {
 });
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', "*:*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
 	res.header("Access-Control-Allow-Credentials", "true")
